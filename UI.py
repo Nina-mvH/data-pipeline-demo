17379c2fd98c6dcfe05a485817461a2b10276a0b
nvhoo <nvhoorn@gmail.com>
2025-04-05 22:33:00 +0000

    added sorting functionality, dates not working

--- UI.py
+++ UI.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc, desc
 from data_pipeline import Base, CaseDB
 
 
@@ -21,11 +21,23 @@ def index():
     if request.method == 'POST':
         loan_number = request.form.get('loan_number')
         borrower_name = request.form.get('borrower_name')
+        sort_by = request.form.get('sort_by')
 
         if loan_number:
-            results = query.filter(CaseDB.loan_number == int(loan_number)).all()
+            query = query.filter(CaseDB.loan_number == int(loan_number))
         elif borrower_name:
-            results = query.filter(CaseDB.borrower_name.like(f"%{borrower_name}%")).all()
+            query = query.filter(CaseDB.borrower_name.ilike(f"%{borrower_name}%"))
+
+        if sort_by == 'name_asc':
+            query = query.order_by(asc(CaseDB.borrower_name))
+        elif sort_by == 'name_desc':
+            query = query.order_by(desc(CaseDB.borrower_name))
+        elif sort_by == 'date_asc':
+            query = query.order_by(asc(CaseDB.date_approved))
+        elif sort_by == 'date_desc':
+            query = query.order_by(desc(CaseDB.date_approved))
+        results = query.all()
+
 
     session.close()
     return render_template('index.html', results=results)
